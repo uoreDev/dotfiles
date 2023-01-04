e4186100ccd3a2a8a9b53736c1b72c835458d316
MinSeok Kim <mskim@yookgak.com>
2023-01-04 20:52:15 +0900

    modify lsp setting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -20,13 +20,18 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'tsserver', 'html', 'cssls', 'gopls' }
+local servers = { 'tsserver', 'html', 'cssls', 'gopls', 'sumneko_lua' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
     capabilities = capabilities,
   }
 end
+
+lspconfig.clangd.setup {
+	cmd = {"/Users/jyg/project/esp/esp-clang/bin/clangd"},
+    capabilities = capabilities,
+}
 
 -- luasnip setup
 local luasnip = require 'luasnip'
