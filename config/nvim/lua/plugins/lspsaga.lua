78958972cf3d5b3cf63578b57274186122677947
MinSeok Kim <uore.dev@gmail.com>
2023-02-05 16:41:07 +0900

    add lspsaga keymap

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,4 +1,6 @@
-require("lspsaga").setup({})
+require('lspsaga').setup({})
 
-vim.keymap.set('n', 'K',         '<cmd>Lspsaga hover_doc<CR>')
-vim.keymap.set('n', '<leader>o', '<cmd>Lspsaga outline<CR>')
+vim.keymap.set( 'n',       'K',         '<cmd>Lspsaga hover_doc<CR>')
+vim.keymap.set( 'n',       '<leader>o', '<cmd>Lspsaga outline<CR>')
+vim.keymap.set({'n', 't'}, '<leader>t', '<cmd>Lspsaga term_toggle<CR>')
+vim.keymap.set( 'n',       '<leader>d', '<cmd>Lspsaga show_line_diagnostics<CR>')
