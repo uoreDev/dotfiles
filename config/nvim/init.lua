d99c12284a6133689701aa52763fdf926971540b
MinSeok Kim <uore.dev@gmail.com>
2023-03-13 16:55:25 +0900

    sumneko_lua(deprecated) -> lua_ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,7 +29,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'tsserver', 'html', 'cssls', 'gopls', 'sumneko_lua', 'vuels' }
+local servers = { 'clangd', 'tsserver', 'html', 'cssls', 'gopls', 'lua_ls', 'vuels' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
