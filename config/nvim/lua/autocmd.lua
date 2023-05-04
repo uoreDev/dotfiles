9373b5693cfd730db552b2396bd8c9814dae673e
MinSeok Kim <uore.dev@gmail.com>
2023-05-04 16:12:00 +0900

    add auto_close NvimTree autocmd when only left

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_command('autocmd VimEnter * NvimTreeToggle')
---- vim.api.nvim_command('autocmd CursorHold * Lspsaga hover_doc')
+vim.api.nvim_command('autocmd BufEnter * ++nested if winnr(\'$\') == 1 && bufname() == \'NvimTree_\' . tabpagenr() | quit | endif')
