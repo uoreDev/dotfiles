223921c371d839ad2eb0e787611e9b18bab35f86
MinSeok Kim <uore.dev@gmail.com>
2023-05-12 18:40:18 +0900

    ignore dap-reple file in copilot

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,5 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_var("copilot_filetypes", {["dap-repl"] = false,})
 
 vim.api.nvim_set_keymap("i", "<C-A>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
