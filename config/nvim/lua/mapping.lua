406b99cc49dd6aa6796fcb7ea52c490ff65a996c
MinSeok Kim <uore.dev@gmail.com>
2024-02-06 00:16:19 +0900

    modify keymap

--- config/nvim/lua/mapping.lua
+++ config/nvim/lua/mapping.lua
@@ -1,3 +1,9 @@
-local bufopts = { noremap=true, silent=true, buffer=bufnr }
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+-- local bufopts = { noremap=true, silent=true, buffer=bufnr }
+-- LSP --
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "declaration" })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition,  { desc = "definition" })
+-- LSP --
+
+-- Git --
+vim.keymap.set('n', '\\gh', '<cmd>Gitsigns preview_hunk_inline<CR>', { desc = "preview hunk" })
+-- Git --
