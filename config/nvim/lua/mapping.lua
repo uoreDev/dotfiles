31219c7bf54c2db6f605bb5bd21e3c3a0bb9a1b5
MinSeok Kim <uore.dev@gmail.com>
2024-03-03 00:29:10 +0900

    add keymap

--- config/nvim/lua/mapping.lua
+++ config/nvim/lua/mapping.lua
@@ -2,10 +2,12 @@
 -- LSP --
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "declaration" })
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition,  { desc = "definition" })
+vim.keymap.set('n', 'K',  vim.lsp.buf.hover,       { desc = "hover" })
 -- LSP --
 
 -- Git --
 vim.keymap.set('n', '\\gg', '<cmd>Neogit<CR>', { desc = "Neogit" })
 
 vim.keymap.set('n', '\\gh', '<cmd>Gitsigns preview_hunk_inline<CR>', { desc = "preview hunk" })
+vim.keymap.set('n', '\\gr', '<cmd>Gitsigns reset_hunk<CR>',          { desc = "reset hunk" })
 -- Git --
