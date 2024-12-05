abc38ff13b1f32a7c777d178a55bd430bdeb4a47
MinSeok Kim <uore.dev@gmail.com>
2024-12-05 17:32:22 +0900

    modify vue lspconfig

--- config/nvim/lua/plugins/lspconfig/languages/vue.lua
+++ config/nvim/lua/plugins/lspconfig/languages/vue.lua
@@ -5,7 +5,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local mason_registry = require('mason-registry')
 local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
 
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   init_options = {
     plugins = {
       {
@@ -19,9 +19,8 @@ lspconfig.tsserver.setup {
 }
 
 -- No need to set `hybridMode` to `true` as it's the default value
-lspconfig.volar.setup {}
+-- lspconfig.volar.setup {}
 
---[[
 lspconfig.volar.setup{
   filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
   init_options = {
@@ -30,4 +29,4 @@ lspconfig.volar.setup{
     }
   },
   capabilities = capabilities,
-}]]
+}
