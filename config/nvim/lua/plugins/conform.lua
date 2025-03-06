7169df6a9cf71b3f61d342876017b4c2be9879a9
MinSeok Kim <uore.dev@gmail.com>
2025-03-06 10:18:28 +0900

    add conform plugin

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,23 @@
 return {
   'stevearc/conform.nvim',
-  opts = {},
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run the first available formatter
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      -- Golang
+      go = { "gofmt" },
+      -- C/C++
+      c = { "clang-format" },
+      cpp = { "clang-format" },
+      -- JSON
+      json = { "jq" },
+    },
+
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
 }
